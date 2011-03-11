ff6e036ccee6aa048e2342020f3bad857866ae36
Gavin <gavinmcdoh@gmail.com>
2011-03-10 21:08:04 -0500

    removed debug list

--- mineConics.js
+++ mineConics.js
@@ -4,7 +4,7 @@ function circle(diameter,color)
 	this.color = color;
 }
 
-function drawGraph($rowList,ctx,row,col,graphScale,x,y)
+function drawGraph(ctx,row,col,graphScale,x,y)
 {
 	ctx.beginPath();
 
@@ -13,21 +13,9 @@ function drawGraph($rowList,ctx,row,col,graphScale,x,y)
 
 	ctx.closePath();
 	ctx.fill();
-
-	// *** DEBUG STUFF *** //
-	var $col = $('<li/>');
-	var $colList = $('<ul/>');
-
-	$col.text('Col ' + col);
-	$col.append($colList);
-	$rowList.append($col);
-
-	$colList.append('<li>X: ' + x + '</li>');
-	$colList.append('<li>Y: ' + (-1 * y) + '</li>');
-	// *** END DEBUG STUFF *** //
 }
 
-function drawCircle($rowList,ctx,row,col,graphScale,x,y,diameter,color)
+function drawCircle(ctx,row,col,graphScale,x,y,diameter,color)
 {
 	var radius = diameter / 2;
 	var circleTest = Math.sqrt((x*x) + (y*y));
@@ -41,40 +29,10 @@ function drawCircle($rowList,ctx,row,col,graphScale,x,y,diameter,color)
 		
 		ctx.closePath();
 		ctx.fill();
-
-		// *** DEBUG STUFF *** //
-		var $col = $('<li/>');
-		var $colList = $('<ul/>');
-
-		$col.text('Col ' + col);
-		$col.append($colList);
-		$rowList.append($col);
-
-		$colList.append('<li>Inside</li>');
-		$colList.append('<li>X: ' + x + '</li>');
-		$colList.append('<li>Y: ' + (-1 * y) + '</li>');
-		$colList.append('<li>Circle Test: ' + circleTest.toFixed(3) + '</li>');
-		// *** END DEBUG STUFF *** //
-	}
-	else
-	{
-		// *** DEBUG STUFF *** //
-		var $col = $('<li/>');
-		var $colList = $('<ul/>');
-
-		$col.text('Col ' + col);
-		$col.append($colList);
-		$rowList.append($col);
-
-		$colList.append('<li>Outside</li>');
-		$colList.append('<li>X: ' + x + '</li>');
-		$colList.append('<li>Y: ' + (-1 * y) + '</li>');
-		$colList.append('<li>Circle Test: ' + circleTest.toFixed(3) + '</li>');
-		// *** END DEBUG STUFF *** //
 	}
 }
 
-function draw(circles,ctx,$debugList)
+function draw(circles,ctx)
 {
 	var graphHeight = $('#graph').height();
 	var graphWidth = $('#graph').width();
@@ -91,83 +49,21 @@ function draw(circles,ctx,$debugList)
 
 	var graphScale = (graphHeight-1) / graphSize; // 'graphHeight-1' so we're not drawing right up to the border
 
-	// *** DEBUG STUFF *** //
-	$debugList.append('<li>Graph Height: ' + graphHeight + '</li>');
-	$debugList.append('<li>Graph Width: ' + graphWidth + '</li>');
-	$debugList.append('<li>Graph Scale: ' + graphScale.toFixed(1) + '</li>');
-	$debugList.slideDown();
-	// *** END DEBUG STUFF *** //
-
-	// *** DEBUG STUFF *** //
-	var drawCtr = 0;
-	var tempID = drawCtr;
-	var $draw = $('<li/>');
-	var $drawAnchor = $('<a/>');
-	var $drawList = $('<ul/>');
-
-	tempID = 'draw' + drawCtr;
-	$draw.addClass('draw');
-	$draw.attr('id',tempID);
-
-	tempID = 'drawAnchor' + drawCtr;
-	$drawAnchor.addClass('draw');
-	$drawAnchor.attr('id',tempID);
-	$drawAnchor.text('test');
-	$drawAnchor.attr('href',$drawAnchor.text());
-
-	tempID = 'drawList' + drawCtr;
-	$drawList.addClass('drawList');
-	$drawList.attr('id',tempID);
-	$drawList.hide();
-
-	$draw.append($drawAnchor);
-	$draw.append($drawList);
-	$debugList.append($draw);
-
-	$drawList.append('<li>Graph Size: ' + graphSize + '</li>');
-	// *** END DEBUG STUFF *** //
-
 	ctx.clearRect(0,0,graphWidth,graphHeight);
 
 	for (var row=0; row<graphSize; row++)
 	{
 		var y = row - ((graphSize-1) / 2);
 
-		// *** DEBUG STUFF *** //
-		var tempID = row;
-		var $row = $('<li/>');
-		var $rowAnchor = $('<a/>');
-		var $rowList = $('<ul/>');
-
-		tempID = 'row' + row;
-		$row.addClass('row');
-		$row.attr('id',tempID);
-
-		tempID = 'rowAnchor' + row;
-		$rowAnchor.addClass('row');
-		$rowAnchor.attr('id',tempID);
-		$rowAnchor.text('row ' + row);
-		$rowAnchor.attr('href',$rowAnchor.text());
-
-		tempID = 'rowList' + row;
-		$rowList.addClass('rowList');
-		$rowList.attr('id',tempID);
-		$rowList.hide();
-
-		$row.append($rowAnchor);
-		$row.append($rowList);
-		$drawList.append($row);
-		// *** END DEBUG STUFF *** //
-
 		for (var col=0; col<graphSize; col++)
 		{
 			var x = col - ((graphSize-1) / 2);
 
-			drawGraph($rowList,ctx,row,col,graphScale,x,y);
+			drawGraph(ctx,row,col,graphScale,x,y);
 
 			$.each(circles,function(index,element)
 			{
-				drawCircle($rowList,ctx,row,col,graphScale,x,y,element.diameter,element.color);
+				drawCircle(ctx,row,col,graphScale,x,y,element.diameter,element.color);
 			});
 		}
 	}
@@ -176,19 +72,15 @@ function draw(circles,ctx,$debugList)
 $(document).ready(function()
 {
 	var circles = [];
-	var $debugList = $('#debugList');
 
 	// get a reference to the canvas
 	var ctx = $('#graph')[0].getContext('2d');
 
 	// display the initial graph
-	draw(circles,ctx,$debugList);
+	draw(circles,ctx);
 
 	$('input.circleSubmit').live('click',function(event)
 	{
-		$debugList.slideUp();
-		$debugList.children().remove();
-
 		// grab the diameter value from the form
 		diameter = parseInt($('input.circleDiameter').val());
 
@@ -202,29 +94,7 @@ $(document).ready(function()
 
 		event.preventDefault();
 
-		draw(circles,ctx,$debugList);
-	});
-
-	$('a.draw').live('click',function(event)
-	{
-		var $draw = $(this).parent();
-		var $drawList = $draw.find('ul.drawList');
-
-		event.preventDefault();
-
-		$draw.siblings().find('ul.drawList:visible').slideUp();
-		$drawList.slideToggle();
-	});
-
-	$('a.row').live('click',function(event)
-	{
-		var $row = $(this).parent();
-		var $rowList = $row.find('ul.rowList');
-
-		event.preventDefault();
-
-		$row.siblings().find('ul.rowList:visible').slideUp();
-		$rowList.slideToggle();
+		draw(circles,ctx);
 	});
 });
 
